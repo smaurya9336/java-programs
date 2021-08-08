111df34e1664b0471e0ed804e310283d33632b91
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2021-08-08 20:46:33 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,9 @@
+public class p2 {
+   public static void main(String[] args) {
+       int a=20;
+       int b=20;
+       int s;
+       s=a+b;
+       System.out.print("Sum of "+ a+ " and "+ b+ " is " +s);
+   } 
+}
