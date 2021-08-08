ea85e033000a51c69ae9bdaa3c1ea53b139e2cda
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2021-08-08 20:45:55 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,10 @@
+public class p1 {
+   public static void main(String[] args) {
+    
+           int a=Integer.parseInt(args[0]);
+           int b=Integer.parseInt(args[1]);
+           int s=a+b;
+           System.out.println("Sum of "+ a+" and"+ b+" is"+s);
+       }
+   } 
+
